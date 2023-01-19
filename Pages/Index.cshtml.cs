f5b31ad5f1f66ed5dccdad778b07dfb49d19078c
kiruch12962 <121095935+kiruch12962@users.noreply.github.com>
2023-01-20 01:34:02 +0300

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Message { get; set; }
+
+        public bool? isOK { get; set; }
+
+        public string UserName { get; set; }
+
+    public void OnGet()
+        {
+
+        }
+        public void OnPost(string inputEmail3, string inputPassword3)
+        {
+            var c = new ICUTechClient();
+            
+            var response = c.LoginAsync(new LoginRequest(inputEmail3, inputPassword3, "101.1.0.2")).Result;
+
+            isOK = !response.@return.Contains("ResultCode");
+
+            UserName = inputEmail3;
+        }
+    }
+}
